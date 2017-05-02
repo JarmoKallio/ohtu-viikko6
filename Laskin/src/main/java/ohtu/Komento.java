9569b29edaf1ba3d02f00e236c318587d5fe6668
Jarmo Kallio <jarmo.kallio@helsinki.fi>
2017-05-02 22:35:42 +0300

    laskin valmis

---
+++ Laskin/src/main/java/ohtu/Komento.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu;
+
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author jambo
+ */
+public abstract class Komento {
+
+    public void peru() {
+
+    }
+
+    public void suorita() {
+
+    }
+    
+}
+
+
